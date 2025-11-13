5843b30e28e0e24b25aee89065c1b99e853bb7c0
Keita8822 <keita8822@icloud.com>
2025-11-13 16:51:47 +0900

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,14 +1,13 @@
-from rq import Worker, Queue, Connection 
-from redis import Redis 
-from app.settings import settings 
-
-def main(): 
-    redis_conn = Redis.from_url(settings.redis_url) 
-    listen = [settings.rq_queue_name] 
+from rq import Worker, Queue, Connection
+from redis import Redis
+from app.settings import settings
+ 
+def main():
+    redis_conn = Redis.from_url(settings.redis_url)
+    listen = [settings.rq_queue_name]
     with Connection(redis_conn):
-         worker = Worker(list(map(Queue, listen))) 
+         worker = Worker(list(map(Queue, listen)))
          worker.work()
-         
-        
-if __name__ == "__main__": 
-    main()
+ 
+ 
+if __name__ == "__main__":
